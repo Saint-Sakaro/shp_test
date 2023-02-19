7d195a4b3262589b640a9a2a09f107f6d1cf438e
Saint-Sakaro <112898876+Saint-Sakaro@users.noreply.github.com>
2023-02-19 20:32:40 +0300

    3

---
+++ views.py
@@ -0,0 +1,122 @@
+import datetime
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import Http404
+from django.shortcuts import render, redirect
+
+from main.forms import LoginForm, AddSnippetForm, ViewSnippetForm
+from main.models import Snippet
+
+from bs4 import BeautifulSoup
+
+
+def get_base_context(request, pagename):
+    return {
+        'pagename': pagename,
+        'loginform': LoginForm(),
+        'user': request.user,
+    }
+
+
+def index_page(request):
+
+    context = get_base_context(request, 'PythonBin')
+    return render(request, 'pages/index.html', context)
+
+
+def add_snippet_page(request):
+    context = get_base_context(request, 'Добавление нового сниппета')
+    if request.method == 'POST':
+        addform = AddSnippetForm(request.POST)
+        if addform.is_valid():
+            record = Snippet(
+                name=addform.data['name'],
+                code=addform.data['code'],
+                creation_date=datetime.datetime.now(),
+            )
+            if request.user.username != "":
+                record.user=request.user
+            record.save()
+            id = record.id
+            messages.add_message(request, messages.SUCCESS, "Сниппет успешно добавлен")
+            return redirect('view_snippet', id=id)
+        else:
+            messages.add_message(request, messages.ERROR, "Некорректные данные в форме")
+            return redirect('add_snippet')
+    else:
+        if request.user.username != "":
+            context['addform'] = AddSnippetForm(
+                initial={
+                    'user': request.user.username
+                }
+            )
+        else:
+            context['addform'] = AddSnippetForm(
+                initial={
+                    'user': "AnonymousUser"
+                }
+            )
+    return render(request, 'pages/add_snippet.html', context)
+
+
+def view_snippet_page(request, id):
+    context = get_base_context(request, 'Просмотр сниппета')
+    try:
+        record = Snippet.objects.get(id=id)
+        if request.user.username != "":
+            context['addform'] = AddSnippetForm(
+                initial={
+                    'user': request.user,
+                    'name': record.name,
+                    'code': record.code,
+                }
+            )
+        else:
+            context['addform'] = AddSnippetForm(
+                initial={
+                    'user': 'AnonymousUser',
+                    'name': record.name,
+                    'code': record.code,
+                }
+            )
+    except Snippet.DoesNotExist:
+        raise Http404
+    return render(request, 'pages/view_snippet.html', context)
+
+
+def login_page(request):
+    if request.method == 'POST':
+        loginform = LoginForm(request.POST)
+        if loginform.is_valid():
+            username = loginform.data['username']
+            password = loginform.data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.add_message(request, messages.SUCCESS, "Авторизация успешна")
+            else:
+                messages.add_message(request, messages.ERROR, "Неправильный логин или пароль")
+        else:
+            messages.add_message(request, messages.ERROR, "Некорректные данные в форме авторизации")
+    return redirect('index')
+
+
+def logout_page(request):
+    logout(request)
+    messages.add_message(request, messages.INFO, "Вы успешно вышли из аккаунта")
+    return redirect('index')
+
+
+def my_snippets_page(request):
+    context = get_base_context(request, 'Просмотр сниппетов')
+    if request.method == 'POST':
+        return redirect('snippets/list')
+    else:
+        snippets = Snippet.objects.all()
+        # TODO: сделать логику записи ответов в голосованиях
+
+        context = {
+            'history': snippets,
+        }
+        return render(request, 'pages/my_snippets.html', context)
